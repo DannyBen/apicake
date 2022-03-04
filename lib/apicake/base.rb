bb5531f82b69a3ef1c4e064ee5c0af4f19a4475a
Danny Ben Shitrit <db@dannyben.com>
2022-03-04 09:00:16 +0000

    fix method_missing for ruby 3.1

--- lib/apicake/base.rb
+++ lib/apicake/base.rb
@@ -53,8 +53,8 @@ module APICake
     #
     #   client.get 'path/optional_sub_path', optional_param: value, optional_param: value
     #
-    def method_missing(method_sym, *arguments, &_block)
-      get "/#{method_sym}", *arguments
+    def method_missing(method_sym, *args, **kwargs)
+      get "/#{method_sym}", *args, **kwargs
     end
 
     # This is the {https://github.com/DannyBen/lightly Lightly} cache object.
